3a8d08ae9da8bd975dc86c374afc753e215c3646
Patrick O <patrick@odenborg.com>
2019-03-16 00:19:51 +0000

    initial commit of v2 of app

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Re-Vents</h1>
+        <p>does hot module work?</p>
+      </div>
+    );
+  }
+}
+
+export default App;
